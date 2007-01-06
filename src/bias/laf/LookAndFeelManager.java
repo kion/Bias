87d63a5a4cd78af51c46de0ef8bb365395655309
kion <info@kion.name>
2007-01-06 19:54:20 +0000

    Look-&-Feel configuration interface implemented

---
+++ src/bias/laf/LookAndFeelManager.java
@@ -0,0 +1,32 @@
+/**
+ * Created on Jan 5, 2007
+ */
+package bias.laf;
+
+import java.util.Properties;
+
+
+/**
+ * @author kion
+ */
+
+public abstract class LookAndFeelManager {
+    
+    /**
+     * Performs needed actions to activate certain Look-&-Feel
+     */
+    public abstract void activate(Properties properties) throws Exception;
+    
+    /**
+     * Configures Look-&-Feel.
+     * By default returns null (no settings).
+     * Should be overriden to return settings for certain Look-&-Feel.
+     * 
+     * @param properties initial settings
+     * @return Properties instance containing configuration settings
+     */
+    public Properties configure(Properties properties) {
+        return null;
+    }
+
+}
